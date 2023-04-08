7733258ee5c7526a45c81c95a16f734841dc9102
MikeGTi <mikegti@ya.com>
2023-04-08 18:26:48 +0300

    Two variants available

--- Main.java
+++ Main.java
@@ -1,54 +1,104 @@
 import java.util.ArrayList;
-
-//package FizzBuzz;
+import java.util.List;
 
 public class Main {
-    public static void main(String args[]) {
-        final int max_num = 100;
-        for (int i = 0; i < max_num; i++) {
-            System.out.println("Hello world!");
+    public static void main(String[] args) {
+
+        final int MAX_NUM = 100;
+
+        //functional programming
+        new FizzBuzzClassic(MAX_NUM).Print();
+
+        //On classes (https://www.youtube.com/watch?v=TWmmfDvcYO0&t=1241s on typeScript)
+        ArrayList<Object> rulesColl = new ArrayList<Object>(List.of(
+        new TagNumRule(new Tag("FizzBuzz"),new AndStrategy(new ArrayList<Object>(List.of(new DividerCondition(3),new DividerCondition(5))))),
+        new TagNumRule(new Tag("Fizz"),new AndStrategy(new ArrayList<Object>(List.of(new DividerCondition(3))))),
+        new TagNumRule(new Tag("Buzz"),new AndStrategy(new ArrayList<Object>(List.of(new DividerCondition(5)))))));
+
+        TagNumRulesCollection numTags;
+        numTags = new TagNumRulesCollection(rulesColl);
+
+        for (int i = 0; i < MAX_NUM; i++) {
+            new Printer(numTags.find(i, String.valueOf(new Tag(i)))).PrintLn();
         }
     }
 
-    abstract static class Condition {
+    static class FizzBuzzClassic {
+        private final int max_num;
+
+        FizzBuzzClassic(int _max_num) {
+            max_num = _max_num;
+        }
+
+        public void Print() {
+            String str1;
+            for (int i = 0; i < max_num; i++) {
+                if (i % 3 == 0 && i % 5 == 0) {
+                    str1 = "FizzBuzz";
+                } else if (i % 3 == 0) {
+                    str1 = "Fizz";
+                } else if (i % 5 == 0) {
+                    str1 = "Buzz";
+                } else {
+                    str1 = String.valueOf(i);
+                }
+                System.out.println(str1);
+            }
+        }
+    }
+}
+
+
+    abstract class Condition {
         public abstract boolean isTruthy(int num);
     }
 
-    public static class Printer {
+    class Printer {
         private String context;
 
         Printer(String _context) {
             this.context = _context;
         }
 
-        void Print() {
-            /*String[] lines = context.split("\n");
+        void PrintLn() {
+            System.out.println(context);
+        }
+
+        void PrintFile(){
+            String[] lines = context.split("\n");
             for(String line : lines){
                 System.out.println(line);
-            }*/
-            System.out.print(context);
+            }
         }
     }
-}
+
     abstract class Strategy implements ITruthy {
-        public abstract Boolean isTruthy();
+        public abstract boolean isTruthy(int num);
         /* extends Strategy */
     }
     interface ITruthy{
-        Boolean isTruthy(int num);
+        boolean isTruthy(int num);
     }
 
 class Tag{
     private String value;
-
-    Tag(String _value ) {
+    //private int value;
+    Tag(int _value ) {
+        //this.value = String.valueOf(_value);
+        this.value = String.valueOf(_value);
+    }
+    Tag(String _value) {
         this.value = _value;
     }
 
     public String getValue() {
         return this.value;
     }
-    public void setValue(String _value) { this.value = _value; }
+
+    @Override
+    public String toString(){
+        return this.value;
+    }
 }
 
 class DividerCondition implements ITruthy {
@@ -60,17 +110,17 @@ class DividerCondition implements ITruthy {
 
     @Override
     public boolean isTruthy(int num) {
-        return ((num % this.divider) == 0);
+        return num % this.divider == 0;
     }
 }
 
-class AndStategy implements ITruthy {
+class AndStrategy extends Strategy {
     private ArrayList<ITruthy> conditionsOrStrategies = new ArrayList();
-    AndStategy() {
+    AndStrategy(ArrayList _conditionsOrStrategies) {
         this.conditionsOrStrategies = _conditionsOrStrategies;
     }
     @Override
-    public Boolean isTruthy(int num) {
+    public boolean isTruthy(int num) {
         for(ITruthy item1 : conditionsOrStrategies){
             if (!item1.isTruthy(num)) {
                 return false;
@@ -78,16 +128,31 @@ class AndStategy implements ITruthy {
         }
         return true;
     }
-
 }
 class TagNumRule {
-    private Tag tag = new Tag();
-    private AndStategy strategy = new AndStategy();
-    TagNumRule(String _tag, AndStategy _strategy){
+    public Tag tag;
+    private Strategy strategy;
+    TagNumRule(Tag _tag, Strategy _strategy){
         this.tag = _tag;
         this.strategy = _strategy;
     }
-    Boolean isSuccess(int num){
+
+    public boolean isSucces(int num) {
         return this.strategy.isTruthy(num);
+    }
+}
+
+class TagNumRulesCollection {
+    private ArrayList<TagNumRule> tags;
+    TagNumRulesCollection(ArrayList<Object> _tag){
+        this.tags = new ArrayList(_tag);
+    }
+    String find(int num, String defaultValue){
+        for(TagNumRule item1 : tags){
+            if (item1.isSucces(num)) {
+                return item1.tag.getValue();
+            }
+        }
+        return defaultValue;
     }
 }
